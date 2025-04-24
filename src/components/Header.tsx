d7ee6198ac171573b6cd70a23a8ba2e891d71aee
Timur Zheksimbaev <113367706+TimurZheksimbaev@users.noreply.github.com>
2025-04-24 22:22:55 +0300

    Fixed issues with SignIn button on adapt on mobile devices

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,41 +1,43 @@
 import { useLocation } from "react-router-dom";
-import brainwave from "../assets/brainwave.svg";
+import { disablePageScroll, enablePageScroll } from "scroll-lock";
+
+import { brainwave } from "../assets";
 import { navigation } from "../constants";
 import { Button } from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
-import { HambugerMenu } from "./design/Header";
+import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
 export const Header = () => {
-  const location = useLocation();
-  const [openNavigation, setopenNavigation] = useState(false);
+  const pathname = useLocation();
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
     if (openNavigation) {
-      setopenNavigation(false);
+      setOpenNavigation(false);
       enablePageScroll();
     } else {
-      setopenNavigation(true);
+      setOpenNavigation(true);
       disablePageScroll();
     }
   };
 
   const handleClick = () => {
     if (!openNavigation) return;
+
     enablePageScroll();
-    setopenNavigation(false);
+    setOpenNavigation(false);
   };
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50  border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center p-1 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
-          <img src={brainwave} alt="Brainwave" width={190} height={40} />
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
 
         <nav
@@ -48,34 +50,35 @@ export const Header = () => {
               <a
                 key={item.id}
                 href={item.url}
+                onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === location.hash
+                  item.url === pathname.hash
                     ? "z-2 lg:text-n-1"
                     : "lg:text-n-1/50"
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
-                onClick={handleClick}
               >
                 {item.title}
               </a>
             ))}
           </div>
 
-          <HambugerMenu />
+          <HamburgerMenu />
         </nav>
 
         <a
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          New Account
+          New account
         </a>
-        {!openNavigation && (
-          <Button className="hidden lg:flex" href="#login">
-            SignIn
+
+        <div className="hidden lg:block">
+          <Button className="flex" href="#login">
+            Sign in
           </Button>
-        )}
+        </div>
 
         <Button
           className="ml-auto lg:hidden"
